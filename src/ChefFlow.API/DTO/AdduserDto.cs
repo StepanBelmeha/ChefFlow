aee214206e8a00f1437bb1870403fa4ff5989a1b
Gasterr <stepan.belmeha@polytech.pp.ua>
2026-04-28 12:45:09 +0300

    added crud operations for user

---
+++ src/ChefFlow.API/DTO/AdduserDto.cs
@@ -0,0 +1,10 @@
+using ChefFlow.API.Models;
+namespace ChefFlow.API.DTO
+{
+    public class AdduserDto
+    {
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+    }
+}
