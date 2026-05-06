195cc6371c0b0a09a6fb20af7815cd7eca358cd0
Gasterr <stepan.belmeha@polytech.pp.ua>
2026-05-06 13:21:36 +0300

    added task controller

---
+++ src/ChefFlow.API/Controllers/UserTasksController.cs
@@ -0,0 +1,79 @@
+namespace ChefFlow.API.Controllers
+{
+    using ChefFlow.API.Data;
+    using ChefFlow.API.DTO;
+    using ChefFlow.API.Models;
+    using Microsoft.AspNetCore.Mvc;
+
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class UserTasksController(AppDbContext context) : ControllerBase
+    {
+        private readonly AppDbContext _context = context;
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var tasks = _context
+                .Tasks
+                .ToList();
+            return Ok(tasks);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var task = _context.Tasks.Find(id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+            return Ok(task);
+        }
+
+        [HttpPost]
+        public IActionResult CreateTask(CreateTaskDTO taskDto)
+        {
+            var task = new UserTask
+            {
+                Title = taskDto.Title,
+                Description = taskDto.Description,
+                UserId = taskDto.UserId,
+                Priority = taskDto.Priority,
+                Deadline = taskDto.Deadline
+            };
+            _context.Tasks.Add(task);
+            _context.SaveChanges();
+            return Ok(task);
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public IActionResult UpdateTask(int id, UpdateTaskDTO taskDto)
+        {
+            var task = _context.Tasks.Find(id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+            task.Deadline = taskDto.Deadline;
+            task.Priority = taskDto.Priority;
+            _context.SaveChanges();
+            return Ok(task);
+        }
+        [HttpDelete]
+        [Route("{id}")]
+        public IActionResult DeleteTask(int id)
+        {
+            var task = _context.Tasks.Find(id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+            _context.Tasks.Remove(task);
+            _context.SaveChanges();
+            return Ok();
+        }
+    }
+}
--- src/ChefFlow.API/DTO/CreateTaskDTO.cs
+++ src/ChefFlow.API/DTO/CreateTaskDTO.cs
@@ -2,6 +2,7 @@ namespace ChefFlow.API.DTO
 {
     public class CreateTaskDTO
     {
+        public int UserId { get; set; }
         public string Title { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public DateTime Deadline { get; set; }
